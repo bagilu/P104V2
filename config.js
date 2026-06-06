7afeee6869f442564dc061956706d9ac2cacf0ef
C. M. Lin <bagilu@gmail.com>
2026-06-06 21:08:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@
 */
 
 window.P104_CONFIG = {
-  SUPABASE_URL: "https://mfljkyvdadxlrbxlboce.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://mfljkyvdadxlrbxlboce.supabase.co/",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mbGpreXZkYWR4bHJieGxib2NlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MTQwMDUsImV4cCI6MjA4MDM5MDAwNX0.Z4OeacVpO8yM1d1uOWZ6jU2Gl7wgEbhXvAFSqF5pBRs"
 };
